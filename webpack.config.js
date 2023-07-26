92ac53e090edb57f9aabb3c4150df14192410b36
robert owuor onyango <owuorrobert76@gmail.com>
2023-07-26 01:48:55 -0700

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   target: 'node', 
-  entry: './index.js', 
+  entry: './app.js', 
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
